490b84a74064925524b0e30a0c553b311b25a9fd
Saúl Páez <saulpaez03@gmail.com>
2023-05-22 11:49:33 -0400

    validate on create songs

--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -9,44 +9,42 @@ import { Genre } from 'src/genres/schemas/genres.schema';
 
 @Injectable()
 export class SongsService {
-  constructor(
-    @InjectModel(Song.name) private songModel: Model<Song>,
-    @InjectModel(Genre.name) private genreModel: Model<Genre>,
-  ) {}
+  constructor(@InjectModel(Song.name) private songModel: Model<Song>) {}
 
   async createNewSong(song: createSongDTO) {
-    if (!song)
-      throw new HttpException('Song must not be empty', HttpStatus.BAD_REQUEST);
-
+    let errorMessages = [];
+    const requiredFields = ['album', 'title', 'data', 'genres'];
+    requiredFields.forEach((key) => {
+      console.log(song[key]);
+      if (song[key] === undefined || '')
+        errorMessages.push(`${key} is required`);
+    });
+    if (errorMessages.length)
+      throw new HttpException(
+        { error: true, errorMessages },
+        HttpStatus.BAD_REQUEST,
+      );
     const genresIds = song.genres.split(',');
+
     const songPath = getMulterFileDataURI(song.data);
 
-    const uploadedSong = await cloudinary.uploader
-      .upload(songPath, {
-        folder: 'spotifySongs',
-        resource_type: 'video',
-      })
-      .catch((e) => {
-        console.log(e);
-        throw new HttpException(
-          'Something went wrong',
-          HttpStatus.INTERNAL_SERVER_ERROR,
-        );
-      });
+    const uploadedSong = await cloudinary.uploader.upload(songPath, {
+      folder: 'spotifySongs',
+      resource_type: 'video',
+    });
 
     if (uploadedSong.error)
       throw new HttpException(
         'Could not uppload audio file',
         HttpStatus.SERVICE_UNAVAILABLE,
       );
-
-    return this.songModel.create({
+    return await this.songModel.create({
       created_at: new Date().toString(),
       data_url: uploadedSong.secure_url,
       duration: uploadedSong.duration,
       genre: genresIds,
       title: song.title,
+      album: song.album,
     });
   }
-  
 }
